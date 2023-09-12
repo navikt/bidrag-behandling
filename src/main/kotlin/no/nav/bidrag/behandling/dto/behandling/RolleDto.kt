6abcadff2e561346ba35bc12c4e8bd34a09b7148
andrii <25188+unorsk@users.noreply.github.com>
2023-09-12 11:29:32 +0200

    Legger til en custom Rolletype serializer

--- src/main/kotlin/no/nav/bidrag/behandling/dto/behandling/RolleDto.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/dto/behandling/RolleDto.kt
@@ -1,12 +1,25 @@
 package no.nav.bidrag.behandling.dto.behandling
 
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import no.nav.bidrag.domain.enums.Rolletype
+import java.io.IOException
 import java.util.Date
 
 data class RolleDto(
     val id: Long,
+    @JsonSerialize(using = RolletypeSerializer::class)
     val rolleType: Rolletype,
     val ident: String,
     val fodtDato: Date?,
     val opprettetDato: Date?,
 )
+
+class RolletypeSerializer : StdSerializer<Rolletype>(Rolletype::class.java) {
+    @Throws(IOException::class)
+    override fun serialize(value: Rolletype, g: JsonGenerator, provider: SerializerProvider) {
+        g.writeString(value.name.toString())
+    }
+}
