6fc81ee7276c81d6f1580de5236ae72f6ae3a5c5
Ugur Cenar <cenar93@gmail.com>
2024-02-27 16:05:00 +0100

    Refaktorering av tester

---
+++ src/main/kotlin/no/nav/bidrag/behandling/controller/v2/VedtakSimuleringController.kt
@@ -0,0 +1,25 @@
+package no.nav.bidrag.behandling.controller.v2
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import no.nav.bidrag.behandling.service.VedtakService
+import no.nav.bidrag.transport.behandling.vedtak.response.VedtakDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@BehandlingRestControllerV2
+class VedtakSimuleringController(
+    private val vedtakService: VedtakService,
+) {
+    @Suppress("unused")
+    @GetMapping("/simulervedtak/{behandlingId}")
+    @Operation(
+        description = "Simuler vedtakstruktur for en behandling. Brukes for testing av grunnlagsstruktur uten å faktisk fatte vedtak",
+        security = [SecurityRequirement(name = "bearer-key")],
+    )
+    fun behandlingTilVedtak(
+        @PathVariable behandlingId: Long,
+    ): VedtakDto {
+        return vedtakService.behandlingTilVedtakDto(behandlingId)
+    }
+}
