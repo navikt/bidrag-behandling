ec2e94e49504d342be501d558e59c2e3a0bcd3c0
Ugur Cenar <cenar93@gmail.com>
2024-02-29 14:22:24 +0100

    Visningsnavn for inntektstype

--- src/main/kotlin/no/nav/bidrag/behandling/controller/VisningsnavnController.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/controller/VisningsnavnController.kt
@@ -2,6 +2,7 @@ package no.nav.bidrag.behandling.controller
 
 import no.nav.bidrag.domene.enums.beregning.Resultatkode
 import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
+import no.nav.bidrag.domene.enums.inntekt.Inntektstype
 import no.nav.bidrag.domene.enums.person.Bostatuskode
 import no.nav.bidrag.domene.enums.person.Sivilstandskode
 import no.nav.bidrag.domene.enums.vedtak.VirkningstidspunktÅrsakstype
@@ -15,6 +16,7 @@ class VisningsnavnController {
     fun hentVisningsnavn(): Map<String, String> {
         return Inntektsrapportering.entries.associate { it.name to it.visningsnavn.intern } +
             VirkningstidspunktÅrsakstype.entries.associate { it.name to it.visningsnavn.intern } +
+            Inntektstype.entries.associate { it.name to it.visningsnavn.intern } +
             Resultatkode.entries.associate { it.name to it.visningsnavn.intern } +
             Bostatuskode.entries.associate { it.name to it.visningsnavn.intern } +
             Sivilstandskode.entries.associate { it.name to it.visningsnavn.intern }
