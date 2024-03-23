392832f821c74fe975d89a2b3e2f52740f39f4ad
s148719 <jan.kjetil.skar@nav.no>
2024-03-23 22:34:12 +0100

    Nytt endepunkt for oppdatering av inntekt

--- src/main/kotlin/no/nav/bidrag/behandling/transformers/Validering.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/transformers/Validering.kt
@@ -1,7 +1,8 @@
 package no.nav.bidrag.behandling.transformers
 
-import no.nav.bidrag.behandling.dto.v2.behandling.OppdatereInntekterRequestV2
-import no.nav.bidrag.behandling.dto.v2.behandling.OppdatereManuellInntekt
+import no.nav.bidrag.behandling.dto.v2.inntekt.OppdatereInntektRequest
+import no.nav.bidrag.behandling.dto.v2.inntekt.OppdatereInntekterRequestV2
+import no.nav.bidrag.behandling.dto.v2.inntekt.OppdatereManuellInntekt
 import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
 import org.springframework.http.HttpStatus
 import org.springframework.web.client.HttpClientErrorException
@@ -13,27 +14,51 @@ private val inntekstrapporteringerSomKreverInnteksttype = listOf(Inntektsrapport
 fun OppdatereInntekterRequestV2.valider() {
     val feilListe = mutableListOf<String>()
     oppdatereManuelleInntekter.forEach { oppdaterInntekt ->
-        if (inntekstrapporteringerSomKreverGjelderBarn.contains(oppdaterInntekt.type)) oppdaterInntekt.validerHarGjelderBarn(feilListe)
-        if (inntekstrapporteringerSomKreverInnteksttype.contains(oppdaterInntekt.type)) oppdaterInntekt.validerHarInnteksttype(feilListe)
+        if (inntekstrapporteringerSomKreverGjelderBarn.contains(oppdaterInntekt.type)) {
+            oppdaterInntekt.validerHarGjelderBarn(
+                feilListe,
+            )
+        }
+        if (inntekstrapporteringerSomKreverInnteksttype.contains(oppdaterInntekt.type)) {
+            oppdaterInntekt.validerHarInnteksttype(
+                feilListe,
+            )
+        }
     }
 
     if (feilListe.isNotEmpty()) {
-        throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "Ugyldig data ved oppdatering av inntekter: ${feilListe.joinToString(", ")}")
+        throw HttpClientErrorException(
+            HttpStatus.BAD_REQUEST,
+            "Ugyldig data ved oppdatering av inntekter: ${feilListe.joinToString(", ")}",
+        )
+    }
+}
+
+fun OppdatereInntektRequest.valider() {
+    val feilListe = mutableListOf<String>()
+    if (inntekstrapporteringerSomKreverGjelderBarn.contains(this.oppdatereManuellInntekt?.type)) {
+        this.oppdatereManuellInntekt?.validerHarGjelderBarn(feilListe)
+    }
+    if (inntekstrapporteringerSomKreverInnteksttype.contains(this.oppdatereManuellInntekt?.type)) {
+        this.oppdatereManuellInntekt?.validerHarInnteksttype(feilListe)
+    }
+
+    if (feilListe.isNotEmpty()) {
+        throw HttpClientErrorException(
+            HttpStatus.BAD_REQUEST,
+            "Ugyldig data ved oppdatering av inntekter: ${feilListe.joinToString(", ")}",
+        )
     }
 }
 
 fun OppdatereManuellInntekt.validerHarGjelderBarn(feilListe: MutableList<String>) {
     if (gjelderBarn == null || gjelderBarn.verdi.isEmpty()) {
-        feilListe.add(
-            "$type må ha en gyldig barnident",
-        )
+        feilListe.add("$type må ha gyldig ident for gjelder barn")
     }
 }
 
 fun OppdatereManuellInntekt.validerHarInnteksttype(feilListe: MutableList<String>) {
     if (inntektstype == null) {
-        feilListe.add(
-            "Barnetillegg må ha en gyldig inntektstype",
-        )
+        feilListe.add("Barnetillegg må ha gyldig inntektstype")
     }
 }
