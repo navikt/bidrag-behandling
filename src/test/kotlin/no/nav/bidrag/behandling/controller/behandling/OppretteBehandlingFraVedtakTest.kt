14ed7c105a0923d5f55024a19f037f154ff6f0dc
Ugur Cenar <cenar93@gmail.com>
2024-04-29 12:10:46 +0200

    Notat feilfiks og støtte for visning av notat i lesemodus

--- src/test/kotlin/no/nav/bidrag/behandling/controller/behandling/OppretteBehandlingFraVedtakTest.kt
+++ src/test/kotlin/no/nav/bidrag/behandling/controller/behandling/OppretteBehandlingFraVedtakTest.kt
@@ -66,7 +66,7 @@ class OppretteBehandlingFraVedtakTest : BehandlingControllerTest() {
         assertSoftly(behandling) {
             roller shouldHaveSize 3
             inntekter shouldHaveSize 15
-            grunnlag shouldHaveSize 33
+            grunnlag shouldHaveSize 34
             refVedtaksid shouldBe 12333
             grunnlag.filter { it.aktiv == null }.shouldHaveSize(10)
             sivilstand shouldHaveSize 2
