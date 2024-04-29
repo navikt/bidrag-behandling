679cc94b89ea5c047767f03918841a505904a702
Ugur Cenar <cenar93@gmail.com>
2024-04-29 11:55:18 +0200

    Notat feilfiks og støtte for visning av notat i lesemodus

--- src/main/kotlin/no/nav/bidrag/behandling/dto/v1/notat/NotatDto.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/dto/v1/notat/NotatDto.kt
@@ -23,7 +23,7 @@ import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.YearMonth
-import java.util.*
+import java.util.Locale
 
 data class NotatDto(
     val saksnummer: String,
