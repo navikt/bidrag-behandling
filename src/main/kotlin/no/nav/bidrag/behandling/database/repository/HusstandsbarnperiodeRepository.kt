67845a4f2938ccc166c09f8b9abf87ce539bf0e0
Ugur Cenar <cenar93@gmail.com>
2024-05-02 08:57:37 +0200

    Fikset konflikter

--- src/main/kotlin/no/nav/bidrag/behandling/database/repository/HusstandsbarnperiodeRepository.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.bidrag.behandling.database.repository
-
-import no.nav.bidrag.behandling.database.datamodell.Husstandsbarnperiode
-import org.springframework.data.repository.CrudRepository
-
-interface HusstandsbarnperiodeRepository : CrudRepository<Husstandsbarnperiode, Long>
