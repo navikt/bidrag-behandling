07c7d29c748e2ff1f7543fe94460949a9360de6f
Ugur Cenar <cenar93@gmail.com>
2024-05-27 20:43:04 +0200

    Lagt til teste

--- src/test/kotlin/no/nav/bidrag/behandling/service/InntektServiceMockTest.kt
+++ src/test/kotlin/no/nav/bidrag/behandling/service/InntektServiceMockTest.kt
@@ -1,20 +1,30 @@
 package no.nav.bidrag.behandling.service
 
+import io.kotest.assertions.any
 import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldContainAnyOf
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import jakarta.persistence.EntityManager
+import no.nav.bidrag.behandling.database.datamodell.Inntekt
+import no.nav.bidrag.behandling.database.grunnlag.SummerteInntekter
 import no.nav.bidrag.behandling.database.repository.BehandlingRepository
 import no.nav.bidrag.behandling.database.repository.InntektRepository
 import no.nav.bidrag.behandling.utils.testdata.opprettInntekt
 import no.nav.bidrag.behandling.utils.testdata.oppretteBehandling
+import no.nav.bidrag.behandling.utils.testdata.oppretteBehandlingRoller
 import no.nav.bidrag.domene.enums.diverse.Kilde
 import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import no.nav.bidrag.transport.behandling.inntekt.response.SummertÅrsinntekt
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.YearMonth
 import java.util.Optional
@@ -35,6 +45,7 @@ class InntektServiceMockTest {
     @BeforeEach
     fun initMock() {
         inntektService = InntektService(behandlingRepository, inntektRepository, entityManager)
+        every { inntektRepository.saveAll<Inntekt>(any()) } answers { firstArg() }
     }
 
     @Test
@@ -96,6 +107,214 @@ class InntektServiceMockTest {
         assertSoftly(inntekter[3]) {
             taMed shouldBe true
             datoFom shouldBe LocalDate.parse("2024-01-01")
+            datoTom shouldBe null
+        }
+    }
+
+    @Test
+    fun `skal lagre inntekter og automatisk ta med offentlige ytelser fra NAV`() {
+        val behandling = oppretteBehandling(1)
+        behandling.roller = oppretteBehandlingRoller(behandling)
+        val summerteInntekter =
+            SummerteInntekter(
+                versjon = "xyz",
+                inntekter =
+                    listOf(
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.BARNETILLEGG,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.UTVIDET_BARNETRYGD,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.UTVIDET_BARNETRYGD,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2024-01"),
+                                    YearMonth.parse("2035-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.SMÅBARNSTILLEGG,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.SMÅBARNSTILLEGG,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    null,
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.AINNTEKT,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.AINNTEKT_BEREGNET_3MND,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.LIGNINGSINNTEKT,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.KONTANTSTØTTE,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                    ),
+            )
+        every { behandlingRepository.findBehandlingById(any()) } returns Optional.of(behandling)
+
+        // hvis
+        inntektService.lagreFørstegangsinnhentingAvSummerteÅrsinntekter(
+            1,
+            personident = Personident(behandling.bidragsmottaker?.ident!!),
+            summerteÅrsinntekter = summerteInntekter.inntekter,
+        )
+
+        val inntekter = behandling.inntekter
+        val inntekterIkkeValgt = inntekter.filter { !it.taMed }
+        val inntekterValgt = inntekter.filter { it.taMed }
+        assertSoftly {
+            inntekterValgt shouldHaveSize 6
+            inntekterIkkeValgt shouldHaveSize 3
+        }
+        assertSoftly(inntekterValgt) {
+            map { it.type }.toSet() shouldContainAnyOf
+                listOf(
+                    Inntektsrapportering.BARNETILLEGG,
+                    Inntektsrapportering.UTVIDET_BARNETRYGD,
+                    Inntektsrapportering.SMÅBARNSTILLEGG,
+                    Inntektsrapportering.KONTANTSTØTTE,
+                )
+            forEach {
+                it.datoFom shouldBe it.opprinneligFom
+            }
+            filter { it.datoTom != null }.forEach {
+                it.datoTom shouldBe it.opprinneligTom
+            }
+            val utvidetBarnetrygd =
+                filter { it.type == Inntektsrapportering.UTVIDET_BARNETRYGD }.sortedBy { it.datoFom }
+            utvidetBarnetrygd.last().datoTom shouldBe null
+        }
+        assertSoftly(inntekterIkkeValgt) {
+            map { it.type } shouldContainAnyOf
+                listOf(
+                    Inntektsrapportering.AINNTEKT,
+                    Inntektsrapportering.AINNTEKT_BEREGNET_3MND,
+                    Inntektsrapportering.LIGNINGSINNTEKT,
+                )
+            forEach {
+                it.datoFom shouldBe null
+                it.datoTom shouldBe null
+            }
+        }
+    }
+
+    @Test
+    fun `skal lagre inntekter og automatisk ta med offentlige ytelser fra NAV og sette datoTom til null hvis etter virkningstidspunkt`() {
+        val behandling = oppretteBehandling(1)
+        behandling.virkningstidspunkt = LocalDate.now().plusMonths(4)
+        behandling.roller = oppretteBehandlingRoller(behandling)
+        val summerteInntekter =
+            SummerteInntekter(
+                versjon = "xyz",
+                inntekter =
+                    listOf(
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.UTVIDET_BARNETRYGD,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.parse("2024-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.UTVIDET_BARNETRYGD,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2023-01"),
+                                    YearMonth.now().plusMonths(2),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                        SummertÅrsinntekt(
+                            inntektRapportering = Inntektsrapportering.UTVIDET_BARNETRYGD,
+                            periode =
+                                ÅrMånedsperiode(
+                                    YearMonth.parse("2024-01"),
+                                    YearMonth.parse("2035-01"),
+                                ),
+                            sumInntekt = BigDecimal(500),
+                        ),
+                    ),
+            )
+        every { behandlingRepository.findBehandlingById(any()) } returns Optional.of(behandling)
+
+        // hvis
+        inntektService.lagreFørstegangsinnhentingAvSummerteÅrsinntekter(
+            1,
+            personident = Personident(behandling.bidragsmottaker?.ident!!),
+            summerteÅrsinntekter = summerteInntekter.inntekter,
+        )
+
+        val inntekter = behandling.inntekter
+        val inntekterValgt = inntekter.filter { it.taMed }.sortedBy { it.datoFom }
+
+        assertSoftly {
+            inntekterValgt shouldHaveSize 3
+        }
+        assertSoftly(inntekterValgt[0]) {
+            datoFom shouldBe opprinneligFom
+            datoTom shouldBe opprinneligTom
+        }
+        assertSoftly(inntekterValgt[1]) {
+            datoFom shouldBe opprinneligFom
+            datoTom shouldBe opprinneligTom
+        }
+        assertSoftly(inntekterValgt[2]) {
+            datoFom shouldBe opprinneligFom
             datoTom shouldBe null
         }
     }
