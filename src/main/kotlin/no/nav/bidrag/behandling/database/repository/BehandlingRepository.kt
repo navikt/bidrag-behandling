cf29d10bbdac340b3ade1cbc6c8ba65d0cc09f0d
s148719 <jan.kjetil.skar@nav.no>
2024-05-30 11:38:07 +0200

    ktlinting

--- src/main/kotlin/no/nav/bidrag/behandling/database/repository/BehandlingRepository.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/database/repository/BehandlingRepository.kt
@@ -4,7 +4,6 @@ import no.nav.bidrag.behandling.database.datamodell.Behandling
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import java.time.LocalDateTime
 import java.util.Optional
 
 interface BehandlingRepository : CrudRepository<Behandling, Long> {
