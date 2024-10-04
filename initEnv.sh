dc69bfb9d6db61312c2bb30c6f35730cfc45a736
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-04 16:02:27 +0200

    Nye miljøer for q1, q2 og q4 (#516)

--- initEnv.sh
+++ initEnv.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 kubectx dev-gcp
-kubectl exec --tty deployment/bidrag-behandling-feature printenv | grep -E 'AZURE_|_URL|SCOPE|UNLEASH' | grep -v -e 'BIDRAG_FORSENDELSE_URL' -e 'BIDRAG_TILGANGSKONTROLL_URL' -e 'BIDRAG_GRUNNLAG_URL' -e 'BIDRAG_VEDTAK_SCOPE' -e 'BIDRAG_VEDTAK_URL' -e 'BIDRAG_BBM_URL' > src/test/resources/application-lokal-nais-secrets.properties
+
+deployment="deployment/bidrag-behandling"
+[ "$1" == "q1" ] && deployment="deployment/bidrag-behandling-feature"
+echo "Henter miljøparametere fra deployment: $deployment"
+kubectl exec --tty $deployment printenv | grep -E 'AZURE_|_URL|SCOPE|UNLEASH' | grep -v -e 'BIDRAG_FORSENDELSE_URL' -e 'BIDRAG_TILGANGSKONTROLL_URL' -e 'BIDRAG_GRUNNLAG_URL' -e 'BIDRAG_VEDTAK_SCOPE' -e 'BIDRAG_VEDTAK_URL' -e 'BIDRAG_STONAD_URL' > src/test/resources/application-lokal-nais-secrets.properties
