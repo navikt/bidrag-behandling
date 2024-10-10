7165659beb95b88279342722dfc266861a27c5e8
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-10 16:36:29 +0200

    Evnevurdering innteksgrense

--- src/main/kotlin/no/nav/bidrag/behandling/service/BeregningService.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/service/BeregningService.kt
@@ -8,12 +8,25 @@ import no.nav.bidrag.behandling.database.datamodell.hentNavn
 import no.nav.bidrag.behandling.dto.v1.beregning.ResultatForskuddsberegningBarn
 import no.nav.bidrag.behandling.dto.v1.beregning.ResultatRolle
 import no.nav.bidrag.behandling.transformers.beregning.validerForSærbidrag
+import no.nav.bidrag.behandling.transformers.finnDelberegningSumLøpendeBidrag
 import no.nav.bidrag.behandling.transformers.vedtak.mapping.tilvedtak.VedtakGrunnlagMapper
+import no.nav.bidrag.beregn.core.bo.Periode
+import no.nav.bidrag.beregn.core.bo.Sjablon
+import no.nav.bidrag.beregn.core.bo.SjablonInnhold
+import no.nav.bidrag.beregn.core.bo.SjablonNøkkel
+import no.nav.bidrag.beregn.core.bo.SjablonPeriode
+import no.nav.bidrag.beregn.core.dto.SjablonPeriodeCore
+import no.nav.bidrag.beregn.core.service.mapper.CoreMapper
 import no.nav.bidrag.beregn.forskudd.BeregnForskuddApi
 import no.nav.bidrag.beregn.særbidrag.BeregnSærbidragApi
-import no.nav.bidrag.domene.enums.beregning.Resultatkode
-import no.nav.bidrag.domene.enums.diverse.Kilde
-import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
+import no.nav.bidrag.beregn.særbidrag.core.bidragsevne.beregning.BidragsevneBeregning
+import no.nav.bidrag.beregn.særbidrag.core.bidragsevne.bo.AntallBarnIHusstand
+import no.nav.bidrag.beregn.særbidrag.core.bidragsevne.bo.BostatusVoksneIHusstand
+import no.nav.bidrag.beregn.særbidrag.core.bidragsevne.bo.GrunnlagBeregning
+import no.nav.bidrag.beregn.særbidrag.core.felles.bo.SjablonListe
+import no.nav.bidrag.commons.service.sjablon.SjablonProvider
+import no.nav.bidrag.domene.enums.person.Bostatuskode
+import no.nav.bidrag.domene.enums.sjablon.SjablonTallNavn
 import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import no.nav.bidrag.transport.behandling.beregning.forskudd.BeregnetForskuddResultat
@@ -66,32 +79,6 @@ class BeregningService(
                 }
             }
         }
-    }
-
-    fun beregnSærbidragInnteksgrense(behandling: Behandling): BigDecimal {
-        val bp = behandling.bidragspliktig ?: return BigDecimal.ZERO
-        behandling.inntekter
-            .filter { it.ident == bp.ident }
-            .forEach { it.taMed = false }
-        (10000000 downTo 100 step 100000).forEach {
-            val inntekt =
-                Inntekt(
-                    taMed = true,
-                    type = Inntektsrapportering.LØNN_MANUELT_BEREGNET,
-                    belop = it.toBigDecimal(),
-                    datoFom = behandling.virkningstidspunkt!!,
-                    datoTom = null,
-                    ident = bp.ident!!,
-                    kilde = Kilde.MANUELL,
-                    behandling = behandling,
-                )
-            behandling.inntekter.add(inntekt)
-            val beregning = beregneSærbidrag(behandling)
-            val resultat = beregning.beregnetSærbidragPeriodeListe.first().resultat
-            if (resultat.resultatkode == Resultatkode.SÆRBIDRAG_MANGLER_BIDRAGSEVNE) return it.toBigDecimal()
-            behandling.inntekter.remove(inntekt)
-        }
-        return BigDecimal.ZERO
     }
 
     fun beregneSærbidrag(behandling: Behandling): BeregnetSærbidragResultat {
@@ -164,4 +151,119 @@ class BeregningService(
                     ),
             ),
         )
+
+    // TODO: For testing av evnevurdering. Skal fjernes når testing er ferdig
+    fun beregnSærbidragInnteksgrense(behandling: Behandling): BigDecimal {
+        val beregning = BidragsevneBeregning()
+        val sjablonListe = hentSjabloner()
+        val beregninResultat = beregneSærbidrag(behandling)
+        val delberegningSumLøpendeBidrag =
+            beregninResultat.grunnlagListe.finnDelberegningSumLøpendeBidrag(
+                beregninResultat.beregnetSærbidragPeriodeListe.first().grunnlagsreferanseListe,
+            )
+        (1..1000000 step 2).forEach {
+            val antallBarnIHusstand =
+                behandling.husstandsmedlem
+                    .filter {
+                        it.perioder.any {
+                            it.bostatus == Bostatuskode.MED_FORELDER ||
+                                it.bostatus == Bostatuskode.DELT_BOSTED
+                        }
+                    }.count()
+            val antallVoksneIHustand =
+                behandling.husstandsmedlem
+                    .filter {
+                        it.perioder.any {
+                            it.bostatus ==
+                                Bostatuskode.BOR_MED_ANDRE_VOKSNE
+                        }
+                    }.count()
+
+            val mapper = object : CoreMapper() {}
+            val sjablontallMap = HashMap<String, SjablonTallNavn>()
+            for (sjablonTallNavn in SjablonTallNavn.entries) {
+                sjablontallMap[sjablonTallNavn.id] = sjablonTallNavn
+            }
+            val sjablonPeriodeCoreListe = ArrayList<SjablonPeriodeCore>()
+
+            sjablonPeriodeCoreListe.addAll(
+                mapper.mapSjablonSjablontall(
+                    beregnDatoFra = behandling.virkningstidspunkt!!,
+                    beregnDatoTil = behandling.virkningstidspunkt!!.plusMonths(1),
+                    sjablonSjablontallListe = sjablonListe.sjablonSjablontallResponse,
+                    sjablontallMap = sjablontallMap,
+                    criteria = { it.bidragsevne },
+                ),
+            )
+            sjablonPeriodeCoreListe.addAll(
+                mapper.mapSjablonBidragsevne(
+                    beregnDatoFra = behandling.virkningstidspunkt!!,
+                    beregnDatoTil = behandling.virkningstidspunkt!!.plusMonths(1),
+                    sjablonBidragsevneListe = sjablonListe.sjablonBidragsevneResponse,
+                ),
+            )
+            sjablonPeriodeCoreListe.addAll(
+                mapper.mapSjablonTrinnvisSkattesats(
+                    beregnDatoFra = behandling.virkningstidspunkt!!,
+                    beregnDatoTil = behandling.virkningstidspunkt!!.plusMonths(1),
+                    sjablonTrinnvisSkattesatsListe = sjablonListe.sjablonTrinnvisSkattesatsResponse,
+                ),
+            )
+            val resultat =
+                beregning.beregn(
+                    GrunnlagBeregning(
+                        inntekt =
+                            no.nav.bidrag.beregn.særbidrag.core.bidragsevne.bo
+                                .Inntekt("", it.toBigDecimal()),
+                        antallBarnIHusstand = AntallBarnIHusstand("", antallBarnIHusstand.toDouble()),
+                        bostatusVoksneIHusstand = BostatusVoksneIHusstand("", antallVoksneIHustand != 0),
+                        sjablonListe = mapSjablonPeriodeListe(sjablonPeriodeCoreListe),
+                    ),
+                )
+            if (resultat.beløp >= delberegningSumLøpendeBidrag!!.sumLøpendeBidrag) return it.toBigDecimal()
+        }
+        return BigDecimal.ZERO
+    }
+
+    protected fun mapSjablonPeriodeListe(sjablonPeriodeListeCore: List<SjablonPeriodeCore>): List<SjablonPeriode> {
+        val sjablonPeriodeListe = mutableListOf<SjablonPeriode>()
+        sjablonPeriodeListeCore.forEach {
+            val sjablonNøkkelListe = mutableListOf<SjablonNøkkel>()
+            val sjablonInnholdListe = mutableListOf<SjablonInnhold>()
+            it.nøkkelListe!!.forEach { nøkkel ->
+                sjablonNøkkelListe.add(SjablonNøkkel(navn = nøkkel.navn, verdi = nøkkel.verdi))
+            }
+            it.innholdListe.forEach { innhold ->
+                sjablonInnholdListe.add(SjablonInnhold(navn = innhold.navn, verdi = innhold.verdi))
+            }
+            sjablonPeriodeListe.add(
+                SjablonPeriode(
+                    sjablonPeriode = Periode(datoFom = it.periode.datoFom, datoTil = it.periode.datoTil),
+                    sjablon = Sjablon(navn = it.navn, nøkkelListe = sjablonNøkkelListe, innholdListe = sjablonInnholdListe),
+                ),
+            )
+        }
+        return sjablonPeriodeListe
+    }
+
+    private fun hentSjabloner(): SjablonListe {
+        // Henter sjabloner for sjablontall
+        val sjablontallListe = SjablonProvider.hentSjablontall()
+
+        // Henter sjabloner for bidragsevne
+        val sjablonBidragsevneListe = SjablonProvider.hentSjablonBidragsevne()
+
+        // Henter sjabloner for samværsfradrag
+        val sjablonSamværsfradragListe = SjablonProvider.hentSjablonSamværsfradrag()
+
+        // Henter sjabloner for trinnvis skattesats
+        val sjablonTrinnvisSkattesatsListe = SjablonProvider.hentSjablonTrinnvisSkattesats()
+
+        return SjablonListe(
+            sjablonSjablontallResponse = sjablontallListe,
+            sjablonBidragsevneResponse = sjablonBidragsevneListe,
+            sjablonTrinnvisSkattesatsResponse = sjablonTrinnvisSkattesatsListe,
+            sjablonSamværsfradragResponse = sjablonSamværsfradragListe,
+        )
+    }
 }
