d5e00c8063ce5540481d3b1b2f69f4bffd9e4721
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-15 10:50:31 +0200

    Samvær

--- src/main/kotlin/no/nav/bidrag/behandling/transformers/samvær/OppdaterSamvær.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/transformers/samvær/OppdaterSamvær.kt
@@ -5,13 +5,20 @@ import no.nav.bidrag.domene.enums.beregning.Samværsklasse
 import no.nav.bidrag.domene.tid.Datoperiode
 
 data class OppdaterSamværDto(
-    val samværId: Long,
     val gjelderBarn: String,
-    val periode: OppdaterSamværPeriodeDto,
+    val periode: OppdaterSamværPeriodeDto? = null,
+    val slettPeriode: Long? = null,
 )
 
 data class OppdaterSamværPeriodeDto(
     val id: Long? = null,
+    val periode: Datoperiode,
+    val samværsklasse: Samværsklasse,
+    val beregning: Samværskalkulator? = null,
+)
+
+data class SamværDto(
+    val gjelderBarn: String,
     val periode: Datoperiode,
     val samværsklasse: Samværsklasse,
     val beregning: Samværskalkulator? = null,
