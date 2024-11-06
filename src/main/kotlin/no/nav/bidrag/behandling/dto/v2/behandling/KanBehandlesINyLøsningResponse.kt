a4f5094ec0b8307459ef8e8a29411ae15731b5d9
Ugur Alpay Cenar <cenar93@gmail.com>
2024-11-06 09:46:09 +0100

    Sjekk om bidrag kan behandles i ny løsning (#576)
    * Sjekk om bidrag kan behandles i ny løsning
    
    * Sjekk om bidrag kan behandles i ny løsning
    
    * Sjekk om bidrag kan behandles i ny løsning
    
    * Sjekk om bidrag kan behandles i ny løsning
    
    * Fikse tester

---
+++ src/main/kotlin/no/nav/bidrag/behandling/dto/v2/behandling/KanBehandlesINyLøsningResponse.kt
@@ -0,0 +1,5 @@
+package no.nav.bidrag.behandling.dto.v2.behandling
+
+data class KanBehandlesINyLøsningResponse(
+    val begrunnelser: List<String> = emptyList(),
+)
--- src/main/kotlin/no/nav/bidrag/behandling/service/ValiderBehandlingService.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/service/ValiderBehandlingService.kt
@@ -3,11 +3,15 @@ package no.nav.bidrag.behandling.service
 import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.behandling.consumer.BidragStønadConsumer
 import no.nav.bidrag.behandling.dto.v2.behandling.KanBehandlesINyLøsningRequest
+import no.nav.bidrag.behandling.dto.v2.behandling.KanBehandlesINyLøsningResponse
 import no.nav.bidrag.behandling.dto.v2.behandling.tilType
 import no.nav.bidrag.commons.util.secureLogger
 import no.nav.bidrag.domene.enums.behandling.TypeBehandling
 import no.nav.bidrag.domene.enums.rolle.Rolletype
+import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import no.nav.bidrag.domene.enums.vedtak.Vedtakstype
 import no.nav.bidrag.transport.behandling.stonad.request.LøpendeBidragssakerRequest
+import no.nav.bidrag.transport.felles.commonObjectmapper
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpClientErrorException
@@ -18,17 +22,49 @@ private val log = KotlinLogging.logger {}
 class ValiderBehandlingService(
     private val bidragStonadConsumer: BidragStønadConsumer,
 ) {
-    fun kanBehandlesINyLøsning(request: KanBehandlesINyLøsningRequest): Boolean {
-        if (request.tilType() == TypeBehandling.SÆRBIDRAG) {
-            val bp = request.roller.find { it.rolletype == Rolletype.BIDRAGSPLIKTIG }!!
-            val løpendeBidrag = bidragStonadConsumer.hentLøpendeBidrag(LøpendeBidragssakerRequest(skyldner = bp.ident!!))
-            return løpendeBidrag.bidragssakerListe.all { it.valutakode == "NOK" || it.valutakode.isEmpty() }
+    fun kanBehandlesINyLøsning(request: KanBehandlesINyLøsningRequest): String? =
+        when (request.tilType()) {
+            TypeBehandling.SÆRBIDRAG -> kanSærbidragBehandlesINyLøsning(request)
+            TypeBehandling.BIDRAG -> kanBidragBehandlesINyLøsning(request)
+            else -> null
         }
-        return true
+
+    private fun kanSærbidragBehandlesINyLøsning(request: KanBehandlesINyLøsningRequest): String? {
+        val bp = request.roller.find { it.rolletype == Rolletype.BIDRAGSPLIKTIG }!!
+        val løpendeBidrag = bidragStonadConsumer.hentLøpendeBidrag(LøpendeBidragssakerRequest(skyldner = bp.ident!!))
+        val harLøpendeBidragUtenlandskValuta =
+            løpendeBidrag.bidragssakerListe.all {
+                it.valutakode == "NOK" || it.valutakode.isEmpty()
+            }
+        return if (!harLøpendeBidragUtenlandskValuta) "Bidragspliktig har løpende bidrag i utenlandsk valuta" else null
+    }
+
+    private fun kanBidragBehandlesINyLøsning(request: KanBehandlesINyLøsningRequest): String? = kanBidragV1BehandlesINyLøsning(request)
+
+    private fun kanBidragV1BehandlesINyLøsning(request: KanBehandlesINyLøsningRequest): String? {
+        if (request.søknadsbarn.size > 1) return "Behandlingen har flere enn ett søknadsbarn"
+        if (request.vedtakstype == Vedtakstype.KLAGE ||
+            request.søknadsreferanseid != null ||
+            request.vedtakRefId != null
+        ) {
+            return "Kan ikke behandle klage eller omgjøring"
+        }
+        val bp = request.bidragspliktig ?: return "Behandlingen mangler bidragspliktig"
+        val harBPMinstEtBidragsstønad =
+            bidragStonadConsumer
+                .hentAlleStønaderForBidragspliktig(bp.ident!!)
+                .stønader
+                .any { it.type != Stønadstype.FORSKUDD }
+        return if (harBPMinstEtBidragsstønad) {
+            "Bidragspliktig har en eller flere historiske eller løpende bidrag"
+        } else {
+            null
+        }
     }
 
     fun validerKanBehandlesINyLøsning(request: KanBehandlesINyLøsningRequest) {
-        if (!kanBehandlesINyLøsning(request)) {
+        val resultat = kanBehandlesINyLøsning(request)
+        if (resultat != null) {
             log.info {
                 "Behandling engangsbeløpstype=${request.engangsbeløpstype}, stønadstype=${request.stønadstype} kan ikke behandles i ny løsning"
             }
@@ -36,6 +72,8 @@ class ValiderBehandlingService(
             throw HttpClientErrorException(
                 HttpStatus.PRECONDITION_FAILED,
                 "Behandling kan ikke behandles i ny løsning",
+                commonObjectmapper.writeValueAsBytes(KanBehandlesINyLøsningResponse(listOf(resultat))),
+                Charsets.UTF_8,
             )
         }
     }
