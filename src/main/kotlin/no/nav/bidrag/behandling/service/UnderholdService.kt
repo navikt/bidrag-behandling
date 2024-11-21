9a47b4a3f641847a64d14b41144a39039c5e8691
s148719 <jan.kjetil.skar@nav.no>
2024-11-21 09:57:43 +0100

    Endre API for oppdatering av begrunnelse og tilsynsordning i underhold

--- src/main/kotlin/no/nav/bidrag/behandling/service/UnderholdService.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/service/UnderholdService.kt
@@ -12,6 +12,7 @@ import no.nav.bidrag.behandling.database.repository.PersonRepository
 import no.nav.bidrag.behandling.database.repository.TilleggsstønadRepository
 import no.nav.bidrag.behandling.database.repository.UnderholdskostnadRepository
 import no.nav.bidrag.behandling.dto.v2.underhold.BarnDto
+import no.nav.bidrag.behandling.dto.v2.underhold.OppdatereBegrunnelseRequest
 import no.nav.bidrag.behandling.dto.v2.underhold.OppdatereFaktiskTilsynsutgiftRequest
 import no.nav.bidrag.behandling.dto.v2.underhold.OppdatereTilleggsstønadRequest
 import no.nav.bidrag.behandling.dto.v2.underhold.OppdatereUnderholdRequest
@@ -21,13 +22,16 @@ import no.nav.bidrag.behandling.dto.v2.underhold.StønadTilBarnetilsynDto
 import no.nav.bidrag.behandling.dto.v2.underhold.UnderholdDto
 import no.nav.bidrag.behandling.dto.v2.underhold.Underholdselement
 import no.nav.bidrag.behandling.transformers.Dtomapper
+import no.nav.bidrag.behandling.transformers.underhold.henteOgValidereUnderholdskostnad
 import no.nav.bidrag.behandling.transformers.underhold.tilStønadTilBarnetilsynDto
 import no.nav.bidrag.behandling.transformers.underhold.validere
 import no.nav.bidrag.behandling.transformers.underhold.validerePerioder
 import no.nav.bidrag.domene.enums.barnetilsyn.Skolealder
 import no.nav.bidrag.domene.enums.diverse.Kilde
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.HttpClientErrorException
 import no.nav.bidrag.transport.behandling.felles.grunnlag.NotatGrunnlag.NotatType as Notattype
 
 @Service
@@ -40,6 +44,45 @@ class UnderholdService(
     private val notatService: NotatService,
     private val dtomapper: Dtomapper,
 ) {
+    fun oppdatereBegrunnelse(
+        behandling: Behandling,
+        request: OppdatereBegrunnelseRequest,
+    ) {
+        val rolleSøknadsbarn =
+            request.underholdsid?.let {
+                henteOgValidereUnderholdskostnad(behandling, it).person.rolle.firstOrNull()
+            }
+
+        notatService.oppdatereNotat(
+            behandling,
+            Notattype.UNDERHOLDSKOSTNAD,
+            request.begrunnelse,
+            rolleSøknadsbarn ?: behandling.bidragsmottaker!!,
+        )
+    }
+
+    @Transactional
+    fun oppdatereTilsynsordning(
+        underholdskostnad: Underholdskostnad,
+        harTilsynsordning: Boolean,
+    ) {
+        if (!harTilsynsordning &&
+            (
+                    underholdskostnad.barnetilsyn.isNotEmpty() ||
+                            underholdskostnad.tilleggsstønad.isNotEmpty() ||
+                            underholdskostnad.faktiskeTilsynsutgifter.isNotEmpty()
+                    )
+        ) {
+            throw HttpClientErrorException(
+                HttpStatus.BAD_REQUEST,
+                "Kan ikke sette harTilsynsordning til usann så lenge barnet er registrert med stønad til barnetilstyn, tilleggsstønad, eller faktiske tilsynsutgift",
+            )
+        }
+
+        underholdskostnad.harTilsynsordning = harTilsynsordning
+    }
+
+    @Deprecated("Erstattes av oppdatereBegrunnelse og oppdatereTilsynsordning")
     @Transactional
     fun oppdatereUnderhold(
         underholdskostnad: Underholdskostnad,
@@ -119,11 +162,11 @@ class UnderholdService(
                             fom = request.periode.fom,
                             tom = request.periode.tom,
                             under_skolealder =
-                                when (request.skolealder) {
-                                    Skolealder.UNDER -> true
-                                    Skolealder.OVER -> false
-                                    else -> null
-                                },
+                            when (request.skolealder) {
+                                Skolealder.UNDER -> true
+                                Skolealder.OVER -> false
+                                else -> null
+                            },
                             omfang = request.tilsynstype,
                             kilde = Kilde.MANUELL,
                             underholdskostnad = underholdskostnad,
@@ -136,7 +179,7 @@ class UnderholdService(
         return OppdatereUnderholdResponse(
             stønadTilBarnetilsyn = oppdatertBarnetilsyn.tilStønadTilBarnetilsynDto(),
             underholdskostnad =
-                dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(underholdskostnad.behandling),
+            dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(underholdskostnad.behandling),
             valideringsfeil = underholdskostnad.barnetilsyn.validerePerioder(),
         )
     }
@@ -176,9 +219,9 @@ class UnderholdService(
         return OppdatereUnderholdResponse(
             faktiskTilsynsutgift = dtomapper.tilFaktiskTilsynsutgiftDto(oppdatertFaktiskTilsynsutgift),
             underholdskostnad =
-                dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(
-                    underholdskostnad.behandling,
-                ),
+            dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(
+                underholdskostnad.behandling,
+            ),
             valideringsfeil = underholdskostnad.barnetilsyn.validerePerioder(),
         )
     }
@@ -215,7 +258,7 @@ class UnderholdService(
         return OppdatereUnderholdResponse(
             tilleggsstønad = dtomapper.tilTilleggsstønadDto(oppdatertTilleggsstønad),
             underholdskostnad =
-                dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(underholdskostnad.behandling),
+            dtomapper.tilUnderholdskostnadsperioderForBehandlingMedKunEttSøknadsbarn(underholdskostnad.behandling),
             valideringsfeil = underholdskostnad.barnetilsyn.validerePerioder(),
         )
     }
