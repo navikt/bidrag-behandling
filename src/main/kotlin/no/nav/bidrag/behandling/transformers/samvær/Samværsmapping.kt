754bb29ad7c1fc98701dab23b542625d571f8722
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-21 07:46:30 +0100

    Lagt til tester

--- src/main/kotlin/no/nav/bidrag/behandling/transformers/samvær/Samværsmapping.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/transformers/samvær/Samværsmapping.kt
@@ -6,10 +6,11 @@ import no.nav.bidrag.behandling.dto.v2.behandling.DatoperiodeDto
 import no.nav.bidrag.behandling.dto.v2.samvær.OppdaterSamværResponsDto
 import no.nav.bidrag.behandling.dto.v2.samvær.SamværDto
 import no.nav.bidrag.behandling.dto.v2.samvær.mapValideringsfeil
-import no.nav.bidrag.behandling.service.NotatService
+import no.nav.bidrag.behandling.service.NotatService.Companion.henteNotatinnhold
 import no.nav.bidrag.behandling.transformers.behandling.tilDto
+import no.nav.bidrag.behandling.transformers.vedtak.takeIfNotNullOrEmpty
 import no.nav.bidrag.beregn.barnebidrag.BeregnSamværsklasseApi
-import no.nav.bidrag.transport.behandling.felles.grunnlag.NotatGrunnlag
+import no.nav.bidrag.transport.behandling.felles.grunnlag.NotatGrunnlag.NotatType
 import java.math.BigDecimal
 
 fun Samvær.tilOppdaterSamværResponseDto() =
@@ -17,18 +18,23 @@ fun Samvær.tilOppdaterSamværResponseDto() =
         oppdatertSamvær = tilDto(),
     )
 
-fun Samvær.tilBegrunnelse() =
-    BegrunnelseDto(NotatService.henteNotatinnhold(behandling, NotatGrunnlag.NotatType.SAMVÆR, rolle), rolle.tilDto())
+fun Samvær.tilBegrunnelse() = BegrunnelseDto(henteNotatinnhold(behandling, NotatType.SAMVÆR, rolle), rolle.tilDto())
 
 fun Samvær.tilBegrunnelseFraOpprinneligVedtak() =
-    BegrunnelseDto(NotatService.henteNotatinnhold(behandling, NotatGrunnlag.NotatType.SAMVÆR, rolle, false), rolle.tilDto())
+    henteNotatinnhold(behandling, NotatType.SAMVÆR, rolle, false)
+        .takeIfNotNullOrEmpty { BegrunnelseDto(it, rolle.tilDto()) }
 
 fun Samvær.tilDto() =
     SamværDto(
         id = id!!,
         gjelderBarn = rolle.ident!!,
         begrunnelse = tilBegrunnelse(),
-        begrunnelseFraOpprinneligVedtak = tilBegrunnelseFraOpprinneligVedtak(),
+        begrunnelseFraOpprinneligVedtak =
+            if (behandling.erKlageEllerOmgjøring) {
+                tilBegrunnelseFraOpprinneligVedtak()
+            } else {
+                null
+            },
         valideringsfeil = mapValideringsfeil().takeIf { it.harFeil },
         perioder =
             perioder
