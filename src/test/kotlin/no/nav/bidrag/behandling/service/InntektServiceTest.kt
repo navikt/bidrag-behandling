649bb85236eaf6288f68df128e315e5d13352543
Ugur Alpay Cenar <cenar93@gmail.com>
2025-02-03 11:24:11 +0100

    Støtte for barnetillegg tiltakspenger

--- src/test/kotlin/no/nav/bidrag/behandling/service/InntektServiceTest.kt
+++ src/test/kotlin/no/nav/bidrag/behandling/service/InntektServiceTest.kt
@@ -333,7 +333,7 @@ class InntektServiceTest : TestContainerRunner() {
                 barnetillegg.belop shouldBe (500).toBigDecimal()
                 barnetillegg.taMed shouldBe true
                 barnetillegg.datoFom shouldBe behandling.virkningstidspunkt
-                barnetillegg.datoTom shouldBe null
+                barnetillegg.datoTom shouldBe LocalDate.parse("2025-01-31")
                 barnetillegg.inntektsposter.size shouldBe 1
                 barnetillegg.inntektsposter.first().inntektstype shouldBe Inntektstype.BARNETILLEGG_PENSJON
                 barnetillegg.inntektsposter.first().beløp shouldBe (500).toBigDecimal()
