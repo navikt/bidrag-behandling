40ad4d822f1168463377519f7abc962f00b6aeab
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-02 11:51:49 +0200

    Feilfiks manuelle vedtak

---
+++ src/main/kotlin/no/nav/bidrag/behandling/controller/v2/AdminController.kt
@@ -0,0 +1,108 @@
+package no.nav.bidrag.behandling.controller.v2
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import no.nav.bidrag.behandling.consumer.BidragSakConsumer
+import no.nav.bidrag.behandling.dto.v1.behandling.OpprettBehandlingRequest
+import no.nav.bidrag.behandling.dto.v1.behandling.OpprettBehandlingResponse
+import no.nav.bidrag.behandling.dto.v1.behandling.OpprettRolleDto
+import no.nav.bidrag.behandling.service.BehandlingService
+import no.nav.bidrag.behandling.service.hentPersonFødselsdato
+import no.nav.bidrag.domene.enums.behandling.BisysSøknadstype
+import no.nav.bidrag.domene.enums.rolle.Rolletype
+import no.nav.bidrag.domene.enums.rolle.SøktAvType
+import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import no.nav.bidrag.domene.enums.vedtak.Vedtakstype
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.LocalDate
+import java.time.Period
+
+private val log = KotlinLogging.logger {}
+
+@BehandlingRestControllerV2
+class AdminController(
+    private val sakConsumer: BidragSakConsumer,
+    private val behandlingService: BehandlingService,
+) {
+    @PostMapping("/admin/opprett/aldersjustering/{saksnummer}")
+    @Operation(
+        description =
+            "Opprett aldersjustering behandling for sak",
+        security = [SecurityRequirement(name = "bearer-key")],
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Forespørsel oppdatert uten feil",
+            ),
+        ],
+    )
+    @Transactional
+    fun opprettAldersjustering(
+        @PathVariable saksnummer: String,
+        @RequestBody barnIdent: String? = null,
+    ): OpprettBehandlingResponse? {
+        val sak = sakConsumer.hentSak(saksnummer)
+        val søknadsbarn =
+            sak.roller
+                .filter { it.type == Rolletype.BARN }
+                .firstOrNull {
+                    listOf(6, 11, 15).contains(getAge(hentPersonFødselsdato(it.fødselsnummer!!.verdi)!!)) ||
+                        barnIdent == it.fødselsnummer!!.verdi
+                }
+                ?: return null
+        val request =
+            OpprettBehandlingRequest(
+                vedtakstype = Vedtakstype.ALDERSJUSTERING,
+                søknadFra = SøktAvType.NAV_BIDRAG,
+                stønadstype = Stønadstype.BIDRAG,
+                søktFomDato = LocalDate.parse("2025-07-01"),
+                mottattdato = LocalDate.parse("2025-07-01"),
+                søknadsid =
+                    BigDecimal
+                        .valueOf(Math.random())
+                        .multiply(BigDecimal.valueOf(1000))
+                        .setScale(0, RoundingMode.HALF_DOWN)
+                        .toLong(),
+                behandlerenhet = sak.eierfogd.verdi,
+                søknadstype = BisysSøknadstype.ALDERSJUSTERING,
+                saksnummer = saksnummer,
+                roller =
+                    setOf(
+                        OpprettRolleDto(
+                            rolletype = Rolletype.BIDRAGSPLIKTIG,
+                            fødselsdato = null,
+                            ident =
+                                sak.roller
+                                    .find { it.type == Rolletype.BIDRAGSPLIKTIG }!!
+                                    .fødselsnummer,
+                        ),
+                        OpprettRolleDto(
+                            rolletype = Rolletype.BIDRAGSMOTTAKER,
+                            fødselsdato = null,
+                            ident =
+                                sak.roller
+                                    .find { it.type == Rolletype.BIDRAGSMOTTAKER }!!
+                                    .fødselsnummer,
+                        ),
+                        OpprettRolleDto(
+                            rolletype = Rolletype.BARN,
+                            fødselsdato = null,
+                            ident = søknadsbarn.fødselsnummer,
+                        ),
+                    ),
+            )
+        return behandlingService.opprettBehandling(request)
+    }
+
+    fun getAge(birthDate: LocalDate): Int = Period.between(birthDate, LocalDate.now()).years
+}
