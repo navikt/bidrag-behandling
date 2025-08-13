61da9d14556aa2bff885b4435c7e0e5203d03ff4
Ugur Alpay Cenar <cenar93@gmail.com>
2025-08-13 10:22:53 +0200

    Oppdater behandling for klagevedtak med innkrevingstype og forbedret håndtering av delvedtak

--- src/main/kotlin/no/nav/bidrag/behandling/transformers/vedtak/mapping/tilvedtak/VedtakGrunnlagMapper.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/transformers/vedtak/mapping/tilvedtak/VedtakGrunnlagMapper.kt
@@ -64,7 +64,7 @@ fun Behandling.finnInnkrevesFraDato(søknadsbarnRolle: Rolle) =
         val beløpshistorikk = hentSisteBeløpshistorikk(tilStønadsid(søknadsbarnRolle))
         beløpshistorikk?.periodeListe?.minOfOrNull { it.periode.fom } ?: YearMonth.from(LocalDate.MAX)
     } else {
-        søknadsbarnRolle.virkningstidspunkt!!.toYearMonth()
+        søknadsbarnRolle.opprinneligVirkningstidspunkt
     }
 
 fun Behandling.finnBeregnTilDatoBehandling(
