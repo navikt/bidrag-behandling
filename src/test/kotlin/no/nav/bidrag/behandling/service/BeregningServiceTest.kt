9df1625510cfe5b5619db3cb8627af19bdd6e5d1
Ugur Alpay Cenar <cenar93@gmail.com>
2025-10-07 09:39:15 +0200

    Avvist behandling

--- src/test/kotlin/no/nav/bidrag/behandling/service/BeregningServiceTest.kt
+++ src/test/kotlin/no/nav/bidrag/behandling/service/BeregningServiceTest.kt
@@ -398,7 +398,7 @@ class BeregningServiceTest {
         assertSoftly(resultat[0]) {
             it.ugyldigBeregning shouldNotBe null
             it.ugyldigBeregning!!.begrunnelse shouldContain "har ingen løpende forskudd"
-            it.ugyldigBeregning!!.perioder shouldHaveSize 1
+            it.ugyldigBeregning!!.perioder shouldHaveSize 2
             it.ugyldigBeregning!!.resultatPeriode[0].type shouldBe UgyldigBeregningType.BEGRENSET_REVURDERING_UTEN_LØPENDE_FORSKUDD
             it.resultat.grunnlagListe
                 .filter { it.type == Grunnlagstype.BELØPSHISTORIKK_FORSKUDD }
@@ -408,7 +408,7 @@ class BeregningServiceTest {
                 .size shouldBe 1
 
             it.barn.ident!!.verdi shouldBe behandling.søknadsbarn.first().ident
-            it.resultat.beregnetBarnebidragPeriodeListe shouldHaveSize 2
+            it.resultat.beregnetBarnebidragPeriodeListe shouldHaveSize 3
             it.resultat.beregnetBarnebidragPeriodeListe[0]
                 .resultat.beløp shouldBe BigDecimal(0)
         }
