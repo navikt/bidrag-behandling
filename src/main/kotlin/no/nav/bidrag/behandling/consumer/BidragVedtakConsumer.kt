216f34f3589da7ecee0ee198ae1687b7df5c2e83
Ugur Alpay Cenar <cenar93@gmail.com>
2025-12-18 10:17:12 +0100

    Støtte for visning av løpende bidrag

--- src/main/kotlin/no/nav/bidrag/behandling/consumer/BidragVedtakConsumer.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/consumer/BidragVedtakConsumer.kt
@@ -1,11 +1,13 @@
 package no.nav.bidrag.behandling.consumer
 
+import no.nav.bidrag.behandling.config.CacheConfig.Companion.MANUELLE_VEDTAK_FOR_BP
 import no.nav.bidrag.behandling.config.CacheConfig.Companion.VEDTAK_CACHE
 import no.nav.bidrag.behandling.config.CacheConfig.Companion.VEDTAK_FOR_STØNAD_CACHE
 import no.nav.bidrag.beregn.barnebidrag.service.external.BeregningVedtakConsumer
 import no.nav.bidrag.commons.cache.BrukerCacheable
 import no.nav.bidrag.commons.util.secureLogger
 import no.nav.bidrag.commons.web.client.AbstractRestClient
+import no.nav.bidrag.transport.behandling.vedtak.request.HentManuelleVedtakRequest
 import no.nav.bidrag.transport.behandling.vedtak.request.HentVedtakForStønadRequest
 import no.nav.bidrag.transport.behandling.vedtak.request.OpprettVedtakRequestDto
 import no.nav.bidrag.transport.behandling.vedtak.response.HentVedtakForStønadResponse
@@ -72,50 +74,62 @@ class BidragVedtakConsumer(
             bidragVedtakUri.pathSegment("hent-vedtak").build().toUri(),
             request,
         )
+
+    @BrukerCacheable(MANUELLE_VEDTAK_FOR_BP)
+    @Retryable(
+        value = [Exception::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 200, maxDelay = 1000, multiplier = 2.0),
+    )
+    override fun hentManuelleVedtak(request: HentManuelleVedtakRequest): HentVedtakForStønadResponse =
+        postForNonNullEntity(
+            bidragVedtakUri.pathSegment("hent-manuelle-vedtak").build().toUri(),
+            request,
+        )
 }
 
-// @Component
-// class BidragVedtakConsumerLocal(
-//    @Value("\${BIDRAG_VEDTAK_LOCAL_URL}") private val bidragVedtakUrl: URI,
-//    @Qualifier("azure") restTemplate: RestTemplate,
-// ) : AbstractRestClient(restTemplate, "bidrag-vedtak") {
-//    private val bidragVedtakUri
-//        get() = UriComponentsBuilder.fromUri(bidragVedtakUrl).pathSegment("vedtak")
-//
-//    fun fatteVedtak(request: OpprettVedtakRequestDto): OpprettVedtakResponseDto =
-//        try {
-//            postForNonNullEntity(
-//                bidragVedtakUri.build().toUri(),
-//                request,
-//            )
-//            //            OpprettVedtakResponseDto((Math.random() * 10000).toInt(), emptyList())
-//        } catch (e: HttpStatusCodeException) {
-//            if (e.statusCode == HttpStatus.CONFLICT) {
-//                val resultat = e.getResponseBodyAs(OpprettVedtakConflictResponse::class.java)!!
-//                secureLogger.info {
-//                    "Vedtak med referanse ${request.unikReferanse} finnes allerede med vedtaksid ${resultat.vedtaksid}."
-//                }
-//                OpprettVedtakResponseDto(resultat.vedtaksid, emptyList())
-//            } else {
-//                secureLogger.error(e) { "Feil ved oppretting av vedtak med referanse ${request.unikReferanse}" }
-//                throw e
-//            }
-//        }
-//
-//    fun hentVedtak(vedtaksid: Int): VedtakDto? =
-//        getForEntity(
-//            bidragVedtakUri.pathSegment(vedtaksid.toString()).build().toUri(),
-//        )
-//
-//    @BrukerCacheable(VEDTAK_FOR_STØNAD_CACHE)
-//    @Retryable(
-//        value = [Exception::class],
-//        maxAttempts = 3,
-//        backoff = Backoff(delay = 200, maxDelay = 1000, multiplier = 2.0),
-//    )
-//    fun hentVedtakForStønad(request: HentVedtakForStønadRequest): HentVedtakForStønadResponse =
-//        postForNonNullEntity(
-//            bidragVedtakUri.pathSegment("hent-vedtak").build().toUri(),
-//            request,
-//        )
-// }
+@Component
+class BidragVedtakConsumerLocal(
+    @Value("\${BIDRAG_VEDTAK_LOCAL_URL}") private val bidragVedtakUrl: URI,
+    @Qualifier("azure") restTemplate: RestTemplate,
+) : AbstractRestClient(restTemplate, "bidrag-vedtak") {
+    private val bidragVedtakUri
+        get() = UriComponentsBuilder.fromUri(bidragVedtakUrl).pathSegment("vedtak")
+
+    fun fatteVedtak(request: OpprettVedtakRequestDto): OpprettVedtakResponseDto =
+        try {
+            postForNonNullEntity(
+                bidragVedtakUri.build().toUri(),
+                request,
+            )
+            //            OpprettVedtakResponseDto((Math.random() * 10000).toInt(), emptyList())
+        } catch (e: HttpStatusCodeException) {
+            if (e.statusCode == HttpStatus.CONFLICT) {
+                val resultat = e.getResponseBodyAs(OpprettVedtakConflictResponse::class.java)!!
+                secureLogger.info {
+                    "Vedtak med referanse ${request.unikReferanse} finnes allerede med vedtaksid ${resultat.vedtaksid}."
+                }
+                OpprettVedtakResponseDto(resultat.vedtaksid, emptyList())
+            } else {
+                secureLogger.error(e) { "Feil ved oppretting av vedtak med referanse ${request.unikReferanse}" }
+                throw e
+            }
+        }
+
+    fun hentVedtak(vedtaksid: Int): VedtakDto? =
+        getForEntity(
+            bidragVedtakUri.pathSegment(vedtaksid.toString()).build().toUri(),
+        )
+
+    @BrukerCacheable(VEDTAK_FOR_STØNAD_CACHE)
+    @Retryable(
+        value = [Exception::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 200, maxDelay = 1000, multiplier = 2.0),
+    )
+    fun hentVedtakForStønad(request: HentVedtakForStønadRequest): HentVedtakForStønadResponse =
+        postForNonNullEntity(
+            bidragVedtakUri.pathSegment("hent-vedtak").build().toUri(),
+            request,
+        )
+}
