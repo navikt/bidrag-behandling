0ad5400372e8e80f861fb7f37cb465ed227dc508
Ugur Alpay Cenar <cenar93@gmail.com>
2026-01-09 13:38:17 +0100

    Håndtering av manglende virkningstidspunkt i rolle tabellen

--- src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Rolle.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Rolle.kt
@@ -126,6 +126,7 @@ open class Rolle(
     @Column(columnDefinition = "jsonb", name = "forholdsmessig_fordeling")
     open var forholdsmessigFordeling: ForholdsmessigFordelingRolle? = null,
 ) {
+    val harSøknadMedInnkreving get() = forholdsmessigFordeling?.søknaderUnderBehandling?.any { it.innkreving } == true
     val erRevurderingsbarn get() = rolletype == Rolletype.BARN && forholdsmessigFordeling != null && forholdsmessigFordeling!!.erRevurdering
     val barn get() =
         behandling.søknadsbarn.filter {
