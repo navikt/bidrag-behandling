81c8e096ec25537709de0768c3f9bc9653c6f341
Ugur Alpay Cenar <cenar93@gmail.com>
2026-02-05 07:57:52 +0100

    Endringer klageberegning

--- src/main/resources/db/migration/V2.52.1__tilleggstonad_belopstype.sql
+++ src/main/resources/db/migration/V2.52.1__tilleggstonad_belopstype.sql
@@ -1,4 +1,4 @@
 alter table tilleggsstønad add column if not exists beløpstype text default 'DAGSATS';
-alter table inntektspost alter column beløpstype set default 'MÅNEDSSATS';
+alter table inntektspost alter column beløpstype set default 'ÅRSBELØP';
 alter table tilleggsstønad alter column dagsats set not null;
-alter table tilleggsstønad drop column månedsbeløp;
+alter table tilleggsstønad drop column if exists månedsbeløp;
