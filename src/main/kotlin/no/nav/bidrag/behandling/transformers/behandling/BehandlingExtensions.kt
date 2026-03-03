5aa82338060f2bfd3b07b6a1ab6c287de71c8eb7
Ugur Alpay Cenar <cenar93@gmail.com>
2026-03-03 08:50:53 +0100

    Feilfiks oppdatering av nye offentlige inntekter når det er flere barn

--- src/main/kotlin/no/nav/bidrag/behandling/transformers/behandling/BehandlingExtensions.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/transformers/behandling/BehandlingExtensions.kt
@@ -1,7 +1,35 @@
 package no.nav.bidrag.behandling.transformers.behandling
 
 import no.nav.bidrag.behandling.database.datamodell.Behandling
+import no.nav.bidrag.behandling.database.datamodell.Rolle
+import no.nav.bidrag.behandling.service.hentPersonFødselsdato
+import no.nav.bidrag.behandling.transformers.dato18ÅrsBidrag
+import no.nav.bidrag.behandling.transformers.erBidrag
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import java.time.LocalDate
+
+fun Behandling.finnRolleForPeriode(
+    ident: String,
+    stønadstype: Stønadstype? = null,
+    periodeFom: LocalDate,
+): Rolle? {
+    val stønadstypeBeregnet =
+        if (stønadstype != null || !this.erBidrag()) {
+            stønadstype
+        } else if (roller.count { it.ident == ident } == 1) {
+            roller.find { it.ident == ident }?.stønadstype
+        } else {
+            val dato18ÅrsBidrag = hentPersonFødselsdato(ident)!!.dato18ÅrsBidrag
+            if (dato18ÅrsBidrag != null && dato18ÅrsBidrag >= periodeFom) {
+                Stønadstype.BIDRAG18AAR
+            } else {
+                Stønadstype.BIDRAG
+            }
+        }
+    return roller.find {
+        it.erSammeRolle(ident, stønadstypeBeregnet)
+    }
+}
 
 fun Behandling.finnRolle(
     ident: String,
