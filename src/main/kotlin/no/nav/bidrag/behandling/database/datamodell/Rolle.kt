0672260e37fb1d5f15c376723572efc91eb7168c
Ugur Alpay Cenar <cenar93@gmail.com>
2026-05-14 16:32:46 +0200

    Refaktorering

--- src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Rolle.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/database/datamodell/Rolle.kt
@@ -17,8 +17,10 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
 import no.nav.bidrag.behandling.database.datamodell.extensions.ÅrsakConverter
 import no.nav.bidrag.behandling.database.datamodell.json.ForholdsmessigFordelingRolle
+import no.nav.bidrag.behandling.database.datamodell.json.ForholdsmessigFordelingSøknadBarn
 import no.nav.bidrag.behandling.dto.grunnlag.PersonStønad
 import no.nav.bidrag.behandling.oppdateringAvBoforholdFeilet
+import no.nav.bidrag.behandling.service.forholdsmessigfordeling.tilFFBarnDetaljer
 import no.nav.bidrag.behandling.service.hentNyesteIdent
 import no.nav.bidrag.behandling.service.hentPersonVisningsnavn
 import no.nav.bidrag.behandling.transformers.Jsonoperasjoner.Companion.jsonListeTilObjekt
@@ -164,6 +166,8 @@ open class Rolle(
         }
     val stønadstypeBarnEllerBehandling get() = stønadstype ?: behandling.stonadstype
     val virkningstidspunktRolle get() = virkningstidspunkt ?: behandling.virkningstidspunktEllerSøktFomDato
+
+    val søknader get() = forholdsmessigFordeling?.søknaderUnderBehandling ?: listOf(behandling.tilFFBarnDetaljer())
 
     fun finnSøknad(søknadsid: Long) =
         forholdsmessigFordeling
