27ff1f4867bffc375d430524624a24c9dd27cebe
Ugur Alpay Cenar <cenar93@gmail.com>
2026-07-22 19:51:26 +0200

    Legg til forholdsmessig fordelingstjeneste i VedtakserviceBidragOmgjøringTest

--- src/main/kotlin/no/nav/bidrag/behandling/service/forholdsmessigfordeling/ForholdsmessigFordelingKravhaverService.kt
+++ src/main/kotlin/no/nav/bidrag/behandling/service/forholdsmessigfordeling/ForholdsmessigFordelingKravhaverService.kt
@@ -356,7 +356,7 @@ class ForholdsmessigFordelingKravhaverService(
             behandling.privatAvtale
                 .filtrerUtPrivatAvtalerSomIkkeErInnenforBeregningsperiode(beregningsperiode)
                 .filter {
-                    it.rolle != null && !kravhavereSomHarÅpenBehandling.map { kravhaver -> kravhaver.kravhaver }.contains(it.personIdent!!)
+                    it.rolle != null && kravhavereSomHarÅpenBehandling.none { kravhaver -> kravhaver.kravhaver == it.personIdent && kravhaver.stønadstype == it.stønadstype }
                 }.mapNotNull { privatAvtale ->
                     val rollePA = privatAvtale.rolle!!
                     val sak = sakerBp.find { it.roller.any { rolle -> rolle.fødselsnummer!!.verdi == rollePA.ident } }!!
